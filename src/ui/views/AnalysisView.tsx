c6bc31f6352ea067837cdd81b87ca45a74dd5ae8
Juno002 <juniorulloa202@gmail.com>
2026-04-19 07:20:36 -0400

    feat: implement AnalysisView component for visualizing patient metrics and cognitive patterns

--- src/ui/views/AnalysisView.tsx
+++ src/ui/views/AnalysisView.tsx
@@ -202,7 +202,6 @@ export default function AnalysisView({ vault }: { vault: Vault }) {
             </p>
           </div>
         </div>
-        </div>
       </div>
 
       {inferredBeliefs && (
